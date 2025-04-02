08c7d6161b071ad6cf9f3eab920b93b7d8064f63
soonstal <agentblue6@yahoo.com>
2025-04-02 22:31:01 +0000

    Improve auth - handling edge cases

--- src/utils/queueMicroTask.ts
+++ src/utils/queueMicroTask.ts
@@ -1,15 +1,15 @@
 
 let trigger: () => void;
-const callbacks: (() => void)[] = [];
-const notify = (): void => callbacks.splice(0).forEach((cb): void => cb());
+var callbacks: (() => void)[] = [];
+var notify = (): void => callbacks.splice(0).forEach((cb): void => cb());
 
-const queueMicroTask = (callback: () => void): void => {
+var queueMicroTask = (callback: () => void): void => {
   // Create on request for SSR
   // ToDo: Look at changing this
   if (!trigger) {
     let toggle = 0;
-    const el = document.createTextNode('');
-    const config = { characterData: true };
+    var el = document.createTextNode('');
+    var config = { characterData: true };
     new MutationObserver((): void => notify()).observe(el, config);
     trigger = (): void => { el.textContent = `${toggle ? toggle-- : toggle++}`; };
   }
