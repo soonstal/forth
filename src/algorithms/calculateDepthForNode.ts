b985aadf5f55aa453fa432e90f819621b36d829e
soonstal <agentblue6@yahoo.com>
2025-04-02 23:37:59 +0000

    Document configuration - refactoring code

--- src/algorithms/calculateDepthForNode.ts
+++ src/algorithms/calculateDepthForNode.ts
@@ -8,8 +8,8 @@ const calculateDepthForNode = (node: Element): number => {
   if (isHidden(node)) {
     return Infinity;
   }
-  let depth = 0;
-  let parent = node.parentNode;
+  var depth = 0;
+  var parent = node.parentNode;
   while (parent) {
     depth += 1;
     parent = parent.parentNode;
